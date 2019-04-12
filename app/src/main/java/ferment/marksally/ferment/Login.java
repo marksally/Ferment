ad493748a6af398a47708512622f900401c21a78
marksally <sally-m@ulster.ac.uk>
2019-04-12 14:50:08 +0100

    Created Login and Registration pages

--- app/src/main/java/ferment/marksally/ferment/Login.java
+++ app/src/main/java/ferment/marksally/ferment/Login.java
@@ -1,13 +1,47 @@
 package ferment.marksally.ferment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button btnRegister;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegister();
+            }
+
+        });
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainMenu();
+            }
+
+        });
+    }
+
+    public void openRegister() {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
+    public void openMainMenu() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ferment/marksally/ferment/Register.java
+++ app/src/main/java/ferment/marksally/ferment/Register.java
@@ -1,13 +1,33 @@
 package ferment.marksally.ferment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+
+    private Button btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnReg = (Button) findViewById(R.id.btnReg);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainMenu();
+            }
+
+        });
     }
+
+    public void openMainMenu() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
